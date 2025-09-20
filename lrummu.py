623be40a2487e618523a50cbc02ade2cc090dd91
Huynh Tri Hai <trihai@Mac.Home>
2025-09-21 00:34:14 +0930

    done

--- lrummu.py
+++ lrummu.py
@@ -1,34 +1,63 @@
 from mmu import MMU
+from collections import OrderedDict
 
 class LruMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for LruMMU
-        pass
+        self.frames = frames
+        self.total_disk_read = 0
+        self.total_disk_write = 0
+        self.total_page_fault = 0
+        self.loaded_pages = OrderedDict() 
+        self.current_size = 0
+        self.debug = False
 
     def set_debug(self):
-        # TODO: Implement the method to set debug mode
-        pass
+        # Implement the method to set debug mode
+        self.debug = True
 
     def reset_debug(self):
-        # TODO: Implement the method to reset debug mode
-        pass
+        # Implement the method to reset debug mode
+        self.debug = False
+
+    def access(self, page_number, is_write):
+        if page_number in self.loaded_pages:
+            if is_write:
+                self.loaded_pages[page_number] = True
+            self.loaded_pages.move_to_end(page_number)
+            if self.debug:
+                print(f"Hit on page {page_number}")
+        else:
+            self.total_page_fault += 1
+            self.total_disk_read += 1
+            if self.current_size == self.frames:
+                evict_page, dirty = self.loaded_pages.popitem(last=False)
+                if dirty:
+                    self.total_disk_write += 1
+                self.current_size -= 1
+                if self.debug:
+                    print(f"Evict page {evict_page}")
+            self.loaded_pages[page_number] = is_write
+            self.loaded_pages.move_to_end(page_number)
+            self.current_size += 1
+            if self.debug:
+                print(f"Load page {page_number}")
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        pass
+        # Implement the method to read memory
+        self.access(page_number, False)
 
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        pass
+        # Implement the method to write memory
+        self.access(page_number, True)
 
     def get_total_disk_reads(self):
-        # TODO: Implement the method to get total disk reads
-        return -1
+        # Implement the method to get total disk reads
+        return self.total_disk_read
 
     def get_total_disk_writes(self):
-        # TODO: Implement the method to get total disk writes
-        return -1
+        # Implement the method to get total disk writes
+        return self.total_disk_write
 
     def get_total_page_faults(self):
-        # TODO: Implement the method to get total page faults
-        return -1
+        # Implement the method to get total page faults
+        return self.total_page_fault
